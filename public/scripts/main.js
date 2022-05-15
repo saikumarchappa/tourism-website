25cb2ad9a58120594b3b4d6445c56b19cec5e88b
saikumarchappa <chappasaikumar6@gmail.com>
2022-05-14 20:48:27 -0400

    Added server Changes

---
+++ public/scripts/main.js
@@ -0,0 +1,62 @@
+// const nav = document.querySelector('nav');
+// if(getCurrentUser()) {
+//   nav.innerHTML = `
+//     <ul>
+//       <li><a href="bmi.html">Calculate</a></li>
+//       <li><a href="profile.html">Profile</a></li>
+//       <li><a id="logout">Logout</a></li>
+//     </ul>
+//   `;
+// } else {
+//   nav.innerHTML = `
+//     <ul>
+//       <li><a href="bmi.html">Calculate</a></li>
+//       <li><a href="login.html">Login</a></li>
+//       <li><a href="register.html">Sign Up</a></li>
+//     </ul>
+//   `
+// }
+
+// http://localhost:3000
+// Fetch method implementation:
+export async function fetchData(route = '', data = {}, methodType) {
+  console.log('http://localhost:8080/'+`${route}`);
+  const response = await fetch('http://localhost:8080'+`${route}`, {
+    method: methodType, // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  if(response.ok) {
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } else {
+    throw await response.json();
+  }
+}
+
+export function setCurrentUser(user) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+export function removeCurrentUser() {
+  localStorage.removeItem('user')
+}
+
+export function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('user'));
+}
+
+export const logoutBtn = document.getElementById("logout");
+if(logoutBtn) logoutBtn.addEventListener('click', logout)
+
+export function logout() {
+  removeCurrentUser();
+  window.location.href = "login.html";
+}
---
+++ public/scripts/user-script.js
@@ -0,0 +1,56 @@
+import 
+{ fetchData, getCurrentUser, setCurrentUser, removeCurrentUser } 
+from './main.js'
+
+const loginForm = document.getElementById("login-form");
+if(loginForm) loginForm.addEventListener('submit', login);
+
+function login(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("username").value;
+  const pswd = document.getElementById("password").value;
+  fetchData('/users/authenticate', {username: name, password: pswd}, "POST")
+  .then((data) => { //cathy123, 12345
+    if(!data.message) {
+      setCurrentUser(data);
+      window.location.href = "./User.html";
+    }else{
+      window.location.href = "./User.html";
+    }
+  })
+  .catch((error) => {
+    const errText = error.message;
+    document.querySelector("#login-form p.error").innerHTML = errText;
+    document.getElementById("pswd").value = "";
+    console.log(`Error! ${errText}`)
+  });
+}
+
+const regForm = document.getElementById("reg-form");
+if(regForm) regForm.addEventListener('submit', register);
+
+function register(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("u_name").value;
+  const pswd = document.getElementById("pswd").value;
+  const f_name = document.getElementById("f_name").value;
+  const l_name = document.getElementById("l_name").value;
+
+  fetchData('/users/register', {username: name, password: pswd, firstName: f_name, lastName: l_name}, "POST")
+  .then((data) => {
+    if(!data.message) {
+      setCurrentUser(data);
+      window.location.href = "./login.html";
+    }else{
+      window.location.href = "./login.html";
+    }
+  })
+  .catch((error) => {
+    const errText = error.message;
+    document.querySelector("#reg-form p.error").innerHTML = errText;
+    document.getElementById("pswd").value = "";
+    console.log(`Error! ${errText}`)
+  });
+}
